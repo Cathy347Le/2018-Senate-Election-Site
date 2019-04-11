0fdb896f03af31ab75b15c4cf3d9bbf6c750cd6b
Cathy <Cathy347Le@gmail.com>
2019-04-11 18:44:16 -0400

    add styling to statusbar

--- src/components/StatusBar/StatusBar.js
+++ src/components/StatusBar/StatusBar.js
@@ -6,10 +6,24 @@ class StatusBar extends Component {
     return (
       <div className="statusbar-container">
         <h4>Results last updated April 9, 10:12 a.m.</h4>
-        <ProgressBar>
-          <ProgressBar animated variant="primary" now={47} key={1} />
-          <ProgressBar animated variant="danger" now={53} key={2} />
-        </ProgressBar>
+        <div className="flex-container">
+          <div className="side-container">
+            <i className="fas fa-democrat fa-3x" />
+            <p className="party-seats">47</p>
+            <p className="party">Democrats</p>
+          </div>
+          <div className="progress-bar-container">
+            <ProgressBar>
+              <ProgressBar animated variant="primary" now={47} key={1} />
+              <ProgressBar animated variant="danger" now={53} key={2} />
+            </ProgressBar>
+          </div>
+          <div className="side-container">
+            <i className="fas fa-republican fa-3x" />
+            <p className="party-seats">53</p>
+            <p className="party">Republicans</p>
+          </div>
+        </div>
       </div>
     );
   }
