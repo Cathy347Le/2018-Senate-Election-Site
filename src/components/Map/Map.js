b8f14cb6904be8b4fdfcd4d7b2b8fae921fc7470
Cathy <Cathy347Le@gmail.com>
2019-04-11 17:44:31 -0400

    added colors to map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import Datamap from "datamaps/dist/datamaps.usa.min";
 import ReactDOM from "react-dom";
 import "./Map.css";
+import MapSenateData from "./senateData.json";
 
 class DataMap extends Component {
   componentDidMount() {
@@ -16,7 +17,7 @@ class DataMap extends Component {
       scope: "usa",
       geographyConfig: {
         highlightBorderColor: "yellow",
-        highlightBorderWidth: 5,
+        highlightBorderWidth: 2,
         popupTemplate: (geography, data) =>
           `<div class='hoverinfo'>
           <strong>
@@ -40,46 +41,10 @@ class DataMap extends Component {
       fills: {
         Republican: "red",
         Democrat: "blue",
+        Independent: "#8A2BE2",
         defaultFill: "lightgray"
       },
-      data: {
-        AZ: {
-          fillKey: "Democrat",
-          stateLong: "Arizona",
-          stateShort: "AZ",
-          demFirst: "Kyrsten",
-          demLast: "Sinema",
-          demVote: "1,191,100",
-          demPercent: 50,
-          repFirst: "Martha",
-          repLast: "McSally",
-          repVote: "1,135,200",
-          repPercent: 47.6,
-          othFirst: "Other",
-          othLast: "candidates",
-          othVote: "57,442",
-          othPercent: 2.4,
-          winner: "demCand"
-        },
-        TX: {
-          fillKey: "Republican",
-          stateLong: "Texas",
-          stateShort: "TX",
-          demFirst: "Beto",
-          demLast: "O'Rourke",
-          demVote: "4,045,632",
-          demPercent: 48.3,
-          repFirst: "Ted",
-          repLast: "Cruz",
-          repVote: "4,260,553",
-          repPercent: 50.9,
-          othFirst: "Other",
-          othLast: "candidates",
-          othVote: "65,470",
-          othPercent: 0.8,
-          winner: "repCand"
-        }
-      }
+      data: MapSenateData
     });
   }
   render() {
