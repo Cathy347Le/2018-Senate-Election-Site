457ee2d68f64e202913c74a7798c344e9bb17536
Cathy <Cathy347Le@gmail.com>
2019-04-11 11:50:11 -0400

    initial draft chart rendered sucess

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Header from "../Header/Header.js";
+import Chart from "../Chart/Chart.js";
 
 class App extends Component {
   render() {
@@ -8,6 +9,9 @@ class App extends Component {
         <header className="header-content">
           <Header />
         </header>
+        <main>
+          <Chart />
+        </main>
       </div>
     );
   }
---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import bb from "billboard.js";
+import "billboard.js/dist/billboard.css";
+
+class Chart extends Component {
+  componentDidMount() {
+    this._renderChart();
+  }
+
+  _renderChart() {
+    bb.generate({
+      bindto: "#chart",
+      data: {
+        columns: [
+          ["data1", 30, 200, 100, 170, 150, 250],
+          ["data2", 130, 100, 140, 35, 110, 50]
+        ],
+        types: {
+          data1: "line",
+          data2: "area-spline"
+        },
+        colors: {
+          data1: "red",
+          data2: "green"
+        }
+      }
+    });
+  }
+
+  render() {
+    return <div id="chart" />;
+  }
+}
+
+export default Chart;
