0447d0b8087f4a7bd7e2859da15d3a35eff3284f
Cathy <Cathy347Le@gmail.com>
2019-04-11 15:52:55 -0400

    render header, map, and graph success

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,10 +6,18 @@ import Map from "../Map/Map.js";
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="map-content">
-          <Map />
-        </div>
+      <div className="App">
+        <header className="header-content">
+          <Header />
+        </header>
+        <main>
+          <div className="map-content">
+            <Map />
+          </div>
+          <div className="graphs-content">
+            <Chart />
+          </div>
+        </main>
       </div>
     );
   }
@@ -18,24 +26,16 @@ class App extends Component {
 export default App;
 
 {
-  /* // class App extends Component {
-//   render() {
-//     return (
-//       <div className="body-content">
-//       <header className="header-content">
-//       <Header />
-//       </header>
-//       < div >
-//       <main>
-//       < div className = "map-content" >
-//         <StateLabelsExample width={960} height={600} />
-//           </div >
-//       <div className="graphs-content">
-//             <Chart />
-//           </div>
-//     </main>
-//       </div >
-//     );
-//   }
-// } */
+  /* 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="map-content">
+          <Map />
+        </div>
+      </div>
+    );
+  }
+} */
 }
