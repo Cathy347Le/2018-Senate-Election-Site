592ec85a33bb8f427b5ee69e96d4427a8b9bc482
Cathy <Cathy347Le@gmail.com>
2019-04-11 10:14:34 -0400

    initial commit; setup app.js

---
+++ src/components/App/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="body-content">
+          <header className="header">
+            <h2>U.S. Senate Election Results 2018</h2>
+            <p>
+              "Republicans are on offense in the Senate, where they’re hoping to
+              add to their current two-seat majority. Ten Democratic senators
+              are facing re-election in states President Trump won in 2016, five
+              of which he took by double digits. To have a chance at controlling
+              the Senate at the end of the night, Democrats will have to cut
+              their losses in these 10 states, pick up seats in states like
+              Nevada and Arizona -- two states that have been trending blue in
+              recent years -- and potentially flip a seat in a traditionally red
+              state like Tennessee or Texas. It’s an uphill climb for Democrats,
+              who lost control of the Senate during the 2014 midterms." -
+              <span className="CNN-source">CNN</span>
+            </p>
+          </header>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
